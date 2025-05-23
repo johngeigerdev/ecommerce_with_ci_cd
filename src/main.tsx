7e018fd086500972f374428cb4d2823e88a4d5ae
John <jgeiger@Mac.lan>
2025-05-22 20:57:35 -0400

    basic functionality working with just React and Typescript

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import '@smastrom/react-rating/style.css'
-import '@smastrom/react-rating/style.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
